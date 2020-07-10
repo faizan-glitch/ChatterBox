5614172050a1d24257efb6f32d0d41d6c9d30792
Faizan <faizan-666@outlook.com>
2020-07-11 01:54:10 +0500

    Fixed the Width Issue for real this time

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,19 +17,19 @@
 
 // deleteUsersRooms();
 
-  function myFunction() {
-      var input, filter, ul, li, a, i, txtValue;
-      input = document.getElementById("searchInput");
-      filter = input.value.toUpperCase();
-      ul = document.getElementById("chatrooms");
-      li = ul.getElementsByTagName("li");
-      for (i = 0; i < li.length; i++) {
-          a = li[i].getElementsByTagName("a")[0];
-          txtValue = a.textContent || a.innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-              li[i].style.display = "";
-          } else {
-              li[i].style.display = "none";
-          }
-      }
-  }
+//   function myFunction() {
+//       var input, filter, ul, li, a, i, txtValue;
+//       input = document.getElementById("searchInput");
+//       filter = input.value.toUpperCase();
+//       ul = document.getElementById("chatrooms");
+//       li = ul.getElementsByTagName("li");
+//       for (i = 0; i < li.length; i++) {
+//           a = li[i].getElementsByTagName("a")[0];
+//           txtValue = a.textContent || a.innerText;
+//           if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//               li[i].style.display = "";
+//           } else {
+//               li[i].style.display = "none";
+//           }
+//       }
+//   }
---
+++ public/scripts/searchRoom.js
@@ -0,0 +1,8 @@
+let searchInput = document.getElementById('searchInput');
+let searchButton = document.getElementById('searchButton');
+console.log(searchButton);
+
+searchButton.addEventListener('click', () => {
+  console.log(searchInput.value);
+  
+})
