cd7198b474309676bcf0c1d6ef03ba778d4c1a22
HamnaFarooq <hamma.farooq@gmail.com>
2020-07-13 14:40:59 +0500

    user can now search anything similar to rooms

--- public/scripts/searchRoom.js
+++ public/scripts/searchRoom.js
@@ -13,18 +13,24 @@ searchButton.addEventListener('click', () => {
       'CSRF-Token': csrfToken
     },
     body: JSON.stringify({
-      name: searchInput.value
+      name: {'$regex': searchInput.value, '$options': 'i'}
     })
   })
   .then(async res => {
     res = await res.json();
     if(res) {
-      $('#searchList').html(`<li class="list-group-item text-left"> ${res.name} </li>`);
+      $('#searchList').html(`<li class="list-group-item text-left"> <a class="nav-link" id="v-pills-home-tab" data-toggle="pill" role="tab" href='#${res.name}'
+      aria-controls="v-pills-home" aria-selected="true">
+      <div class="row justify-content-between px-1">
+        <span> ${res.name} </span>
+        <span class="badge badge-pilsl badge-danger d-flex align-items-center">1</span>
+      </div>
+    </a> </li>`);
     }
     else {
       $('#searchList').html(`<li class="list-group-item text-left"> No Room Found. <i class="fa fa-frown" aria-hidden="true"></i> </li>`);
     }
-    $('#collapseSearch').collapse();
+    $('#collapseSearch').collapse();  //! this is not working!!!!
   })
   .catch(err => {
     console.log(err);       
